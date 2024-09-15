261abf7bd30a0c5557faf719762408fb4ef20dac
Ivan Sanchez <ivansanchezp98@gmail.com>
2024-09-15 19:49:32 +0200

    ✨: grid template

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,30 +2,32 @@ import { useNavigate } from 'react-router-dom';
 
 import { Gear } from '../../assets/icons/Gear';
 import withTransition from '../../HOC/withTransition';
+import { Spotify } from '../../assets/icons/Spotify';
 
 function Home() {
   const navigate = useNavigate();
 
   return (
     <>
-      {/* Mobile Header */}
-      <header className="block lg:hidden p-3">
-        <div className="flex justify-between items-center">
-          <span className="font-semibold text-2xl">Buenas tardes</span>
-          <button className="p-3" onClick={() => navigate('/settings')}>
-            <Gear width={'1.5em'} height={'1.5em'} />
-          </button>
+      <div className="h-screen grid grid-cols-[auto_1fr] grid-rows-[auto_1fr_auto] gap-2 p-2 overflow-hidden">
+        <div className="col-span-3 rounded-lg">
+          <div className="flex justify-between items-center h-[48px]">
+            <div className="w-[72px] h-[72px] flex items-center justify-center">
+              <a className="flex items-center">
+                <Spotify width={'32px'} height={'32px'} />
+              </a>
+            </div>
+            <div>Busqueda</div>
+            <button className="p-3" onClick={() => navigate('/settings')}>
+              <Gear width={'1.5em'} height={'1.5em'} />
+            </button>
+          </div>
         </div>
-      </header>
-
-      {/* Desktop Grid */}
-      <div className="hidden lg:grid lg:grid-cols-3 lg:h-screen lg:gap-2 lg:p-2 overflow-hidden">
-        <div className="grid grid-rows-4 gap-2 h-screen">
-          <div className="bg-[#1e1e1e] p-4 rounded-lg">Item 1.1</div>
-          <div className="bg-[#1e1e1e] p-4 row-span-3 rounded-lg">Item 1.2</div>
+        <div className="w-[72px] lg:w-[280px] row-span-2 bg-[#1e1e1e] p-4 rounded-lg">
+          Item 2
         </div>
-        <div className="row-span-2 bg-[#1e1e1e] p-4 rounded-lg">Item 2</div>
-        <div className="row-span-2 bg-[#1e1e1e] p-4 rounded-lg">Item 3</div>
+        <div className="bg-[#1e1e1e] p-4 rounded-lg">Item 3</div>
+        <div className="w-[280px] bg-[#1e1e1e] p-4 rounded-lg">Item 4</div>
       </div>
     </>
   );
